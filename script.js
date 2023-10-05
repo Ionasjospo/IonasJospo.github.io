5f79343d0bd60ea87694e910bbe7c5866452d1a5
IonasJospo <ionas.josponis@correo.ucu.edu.uy>
2023-10-05 15:54:11 -0300

    fin

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
-// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén todas las diapositivas
+    var slides = document.querySelectorAll('.carousel-slide');
 
+    // Configura un índice de diapositiva actual
+    var currentSlide = 0;
 
-// Obtén todas las diapositivas y botones
-var slides = document.querySelectorAll('.carousel-slide');
-var prevBtn = document.getElementById('prevBtn');
-var nextBtn = document.getElementById('nextBtn');
-
-// Configura un índice de diapositiva actual
-var currentSlide = 0;
-
-// Función para mostrar la diapositiva actual
-function showSlide(n) {
-    // Oculta todas las diapositivas
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
+    // Función para mostrar la diapositiva actual
+    function showSlide(n) {
+        // Oculta todas las diapositivas
+        for (var i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+        // Muestra la diapositiva deseada
+        slides[n].style.display = 'block';
     }
-    // Muestra la diapositiva deseada
-    slides[n].style.display = 'block';
-}
 
-// Función para avanzar a la siguiente diapositiva
-function nextSlide() {
-    currentSlide++;
-    if (currentSlide >= slides.length) {
-        currentSlide = 0; // Vuelve al principio si alcanza el final
+    // Función para avanzar a la siguiente diapositiva
+    function nextSlide() {
+        currentSlide++;
+        if (currentSlide >= slides.length) {
+            currentSlide = 0; // Vuelve al principio si alcanza el final
+        }
+        showSlide(currentSlide);
     }
+
+    // Mostrar la primera diapositiva al cargar la página
     showSlide(currentSlide);
-}
 
-// Función para retroceder a la diapositiva anterior
-function prevSlide() {
-    currentSlide--;
-    if (currentSlide < 0) {
-        currentSlide = slides.length - 1; // Ir al final si está en el principio
-    }
-    showSlide(currentSlide);
-}
-
-// Mostrar la primera diapositiva al cargar la página
-showSlide(currentSlide);
-
-// Configurar avance automático (cambia de diapositiva cada 3 segundos)
-setInterval(nextSlide, 3000);
-
-// Agregar eventos de clic a los botones de anterior y siguiente
-prevBtn.addEventListener('click', prevSlide);
-nextBtn.addEventListener('click', nextSlide);
+    // Configurar avance automático (cambia de diapositiva cada 3 segundos)
+    setInterval(nextSlide, 3000);
+});
